221f9eb7e66b0a0ecb04bc2f4dbb2dc17a4e5ea0
Pradeep M Murjwani <pradeep.m.murjwani@gmail.com>
2020-07-21 19:38:04 +0530

    Calculator Services and their test cases

--- src/main/java/com/devops/bootcamp/CalculatorService.java
+++ src/main/java/com/devops/bootcamp/CalculatorService.java
@@ -1,6 +1,7 @@
 package com.devops.bootcamp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,31 @@ public class CalculatorService {
 	public String sayHello() {
 		return "Hello";
 	}
+	
+	@GetMapping("/welcome")
+	public String welcomeMessage(@RequestParam String name) {
+		return "Welcome " + name;
+	}
+	
+	@GetMapping("/sum")
+	public int sum(@RequestParam int a, @RequestParam int b) {
+		return a+b;
+	}
+	
+	@GetMapping("/subtract")
+	public int subtract(@RequestParam int a, @RequestParam int b) {
+		return a-b;
+	}
+	
+	@GetMapping("/multiply")
+	public int multiply(@RequestParam int a, @RequestParam int b) {
+		return a*b;
+	}
+	
+	@GetMapping("/divide")
+	public float divide(@RequestParam float a, @RequestParam float b) {
+		return a/b;
+	}
 }
+
+
